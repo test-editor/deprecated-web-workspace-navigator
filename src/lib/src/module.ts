35dbe2035c391cc88d1e251819564a082a41d8f3
Franz Becker <franz.becker@signal-iduna.de>
2017-07-26 14:29:23 +0200

    add navigator code from https://github.com/test-editor/test-editor-web and include it into the demo

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavigationComponent } from './component/navigation/navigation.component';
+import { TreeViewerComponent } from './component/tree-viewer/tree-viewer.component';
+
+import { WorkspaceService } from './service/workspace/workspace.service';
 
 import { LibComponent } from './component/lib.component';
 import { LibService } from './service/lib.service';
 
 @NgModule({
-  declarations: [LibComponent],
-  providers: [LibService],
-  exports: [LibComponent]
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavigationComponent,
+    TreeViewerComponent,
+    LibComponent
+  ],
+  providers: [
+    WorkspaceService,
+    LibService
+  ],
+  exports: [
+    NavigationComponent,
+    TreeViewerComponent,
+    LibComponent
+  ]
 })
 export class LibModule { }
