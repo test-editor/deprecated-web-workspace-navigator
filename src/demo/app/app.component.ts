6e5950278d413c3642e7d4b6411c0b2c2c01ef08
Franz Becker <franz.becker@signal-iduna.de>
2017-08-15 14:40:48 +0200

    improve demo styling

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -4,7 +4,8 @@ import { WorkspaceDocument } from '@testeditor/workspace-navigator';
 
 @Component({
   selector: 'demo-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
