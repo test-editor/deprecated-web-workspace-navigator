a9059d2a8b09ab31eb06dd91344287bc94ec0e20
Franz Becker <franz.becker@signal-iduna.de>
2017-08-16 14:01:47 +0200

    remove LibComponent, LibService and improve integration test

--- integration/src/app/app.component.ts
+++ integration/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { LibService } from '@testeditor/workspace-navigator';
+import { MessagingService } from '@testeditor/messaging-service';
+import { WorkspaceDocument } from '@testeditor/workspace-navigator';
 
 @Component({
   selector: 'integration-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  meaning: number;
-  constructor(libService: LibService) {
-    this.meaning = libService.getMeaning();
+
+    lastDocument: WorkspaceDocument;
+
+    constructor(messagingService: MessagingService) {
+      messagingService.subscribe('navigation.open', (model: WorkspaceDocument) => {
+        console.log(`Received 'navigation.open' on '${model.name}'.`);
+        this.lastDocument = model;
+      });
+    }
   }
-}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,4 @@
-export { LibComponent } from './src/component/lib.component';
-export { LibService } from './src/service/lib.service';
 export { WorkspaceNavigatorModule } from './src/workspace.navigator.module';
-
 export { NavigationComponent } from './src/component/navigation/navigation.component';
 export { TreeViewerComponent } from './src/component/tree-viewer/tree-viewer.component';
 export { PersistenceService } from './src/service/persistence/persistence.service';
