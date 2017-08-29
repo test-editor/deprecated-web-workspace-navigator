da9bbe57a237c3691cb59c8c83f1311f2f6d45cc
Franz Becker <franz.becker@signal-iduna.de>
2017-08-29 16:32:33 +0200

    improve demo project

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { MessagingService } from '@testeditor/messaging-service';
 
 import * as events from '@testeditor/workspace-navigator';
+import { EDITOR_DIRTY_CHANGED, EDITOR_ACTIVE, EDITOR_CLOSE } from '../../lib/src/component/event-types';
 
 @Component({
   selector: 'demo-app',
@@ -12,14 +13,45 @@ export class AppComponent {
 
   lastSelected: any;
   lastOpened: any;
+  paths: string[] = [];
 
-  constructor(messagingService: MessagingService) {
-    messagingService.subscribe(events.NAVIGATION_SELECT, element => {
+  constructor(private messagingService: MessagingService, private changeDetectorRef: ChangeDetectorRef) {
+    this.subscribeToEvents();
+  }
+
+  private subscribeToEvents(): void {
+    this.messagingService.subscribe(events.NAVIGATION_SELECT, element => {
       this.lastSelected = element;
     });
-    messagingService.subscribe(events.NAVIGATION_OPEN, element => {
+    this.messagingService.subscribe(events.NAVIGATION_OPEN, element => {
       this.lastOpened = element;
+      if (element.path && this.paths.indexOf(element.path) < 0) {
+        this.paths.push(element.path);
+        this.changeDetectorRef.detectChanges();
+      }
     });
+    this.messagingService.subscribe(events.NAVIGATION_DELETED, element => {
+      this.close(element.path);
+    });
+  }
+
+  setDirty(path: string, dirty: boolean): void {
+    this.messagingService.publish(events.EDITOR_DIRTY_CHANGED, {
+      path: path,
+      dirty: dirty
+    });
+  }
+
+  setActive(path: string): void {
+    this.messagingService.publish(events.EDITOR_ACTIVE, { path: path });
+  }
+
+  close(path: string): void {
+    let index = this.paths.indexOf(path);
+    if (index >= 0) {
+      this.messagingService.publish(events.EDITOR_CLOSE, { path: path });
+      this.paths.splice(index, 1);
+    }
   }
 
 }
