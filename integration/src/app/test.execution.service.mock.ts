792a19e80d740cc4a8525456712a5c1cc5e92034
Jan Jelschen <jan.jelschen@itemis.de>
2018-01-26 16:10:35 +0100

    add mock of test exec service for itest app

---
+++ integration/src/app/test.execution.service.mock.ts
@@ -0,0 +1,43 @@
+import { Response, ResponseOptions } from '@angular/http';
+import { AuthHttp } from 'angular2-jwt';
+import { Injectable } from '@angular/core';
+import { ElementState } from '@testeditor/workspace-navigator';
+
+@Injectable()
+export class TestExecutionServiceMock {
+private readonly statusMap = new Map<string, string>();
+
+  execute(path: string): Promise<Response> {
+    console.log(`Received execute(path: '${path}')`);
+    this.statusMap[path] = 'RUNNING';
+    return Promise.resolve(new Response(new ResponseOptions({ status: 201 })));
+  }
+
+  status(path: string): Promise<Response> {
+    console.log(`Received status(path: '${path}')`);
+    let timeout: number;
+    if (this.statusMap[path] === 'RUNNING') {
+      let self = this;
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(new Response(new ResponseOptions({
+            status: 200,
+            body: 'RUNNING'
+          })));
+        }, 5000);
+        self.statusMap[path] = Math.random() < 0.5 ? 'SUCCESS' : 'FAILED';
+        console.log(`Test '${path}' will complete with status '${self.statusMap[path]}'`);
+      });
+    } else {
+      return Promise.resolve(new Response(new ResponseOptions({
+            status: 200,
+            body: this.statusMap[path]
+          })));
+    }
+  }
+
+  statusAll(): Promise<Map<string, ElementState>> {
+    return Promise.resolve(new Map<string, ElementState>([]));
+  }
+
+}
