8a3e277a22e07ef6efa7f93fe80725038ec9f7af
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-02-01 18:45:25 +0100

    added integration test to check font-awesome element

--- integration/e2e/app.e2e-spec.ts
+++ integration/e2e/app.e2e-spec.ts
@@ -18,4 +18,21 @@ describe('Workspace Navigator tests', function () {
     expect(navigation).toBeTruthy();
   });
 
+  it('should know about font awesome icons', () => {
+    // when (not in aot test, since index-aot.html there is no css included as in index.html)
+    let heightPromise = browser.executeScript(
+      'var result = 1; \
+       if (window.module != "aot") { \
+         result = parseInt(window.getComputedStyle(document.getElementById("new-file", null)).getPropertyValue("height"), 10); \
+       } \
+       return result;'
+    )
+
+    // then
+    heightPromise.then((height) => {
+      console.log("found icon to be of height " + height);
+      expect(height).toBeGreaterThan(0); // is 0 if the font is not found!
+    });
+  });
+
 });
