710c7cf05ac87e7ef1ab02ea4b63cc9d53ee8034
Jan Jelschen <jan.jelschen@itemis.de>
2018-03-05 09:49:38 +0100

    refactor in response to PR #58 remarks

--- src/lib/src/common/markers/workspace.marker.update.ts
+++ src/lib/src/common/markers/workspace.marker.update.ts
@@ -1,4 +1,15 @@
 export class WorkspaceMarkerUpdate {
   path: string;
+  /**
+   * Arbitrary Javascript object, whose attributes will be stored as marker fields associated with this update's path.
+   * For example, if the object is
+   * ```
+   * {
+   *   "status": "RUNNING",
+   *   "name": "test.tcl"
+   * }
+   * ```
+   * then markers named 'status' and 'name' will be created, and assigned the values 'RUNNING' and 'test.tcl', respectively.
+   */
   markers: any;
 }
