f58840829f010a76b14b630e3f81ee0c1981be4c
Jan Jelschen <jan.jelschen@itemis.de>
2018-04-03 17:02:14 +0200

    remove chrome from karma test execution

--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -7,7 +7,6 @@ module.exports = function (config) {
   config.set({
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
       require('karma-firefox-launcher'), 
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
--- karma.conf.js
+++ karma.conf.js
@@ -96,7 +96,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome_travis_ci', 'Firefox'],
+    browsers: ['Firefox'],
     singleRun: false
   })
 }
